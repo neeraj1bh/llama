e44164190b1838de28f54f05715984298836c33c
Neeraj Bhatt <neeraj1bh@gmail.com>
2024-05-07 11:32:42 +0530

    chore: init client

---
+++ client/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="bg-orange-100 flex h-screen w-screen">
+      <div className="m-auto text-3xl">Hello, World!</div>
+    </div>
+  );
+}
+
+export default App;
