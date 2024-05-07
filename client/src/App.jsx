ab3974d535ba9e57b3d8693c005df9461e3a0dcc
Neeraj Bhatt <neeraj1bh@gmail.com>
2024-05-07 12:04:51 +0530

    chore: added basic design

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,45 @@
+function ChatMessage({ message, type }) {
+  return (
+    <div
+      className={`flex w-full ${
+        type === "sent" ? "justify-start" : "justify-end"
+      }`}
+    >
+    <div
+        className={`${
+          type === "sent"
+            ? "bg-violet-400  text-white rounded-tl-lg"
+            : "bg-white text-black rounded-tr-lg"
+        } p-2 rounded-b-lg `}
+      >
+        {message}
+      </div>
+    </div>
+  );
+}
+
 function App() {
   return (
-    <div className="bg-orange-100 flex h-screen w-screen">
-      <div className="m-auto text-3xl">Hello, World!</div>
+    <div className="p-5 h-screen bg-black">
+      <div className="container mx-auto bg-gray-900 h-full flex flex-col">
+        <div className="flex-1 flex flex-row items-end p-3">
+          <div className="w-full">
+            <ChatMessage message="Hello how are you?" type="sent" />
+            <ChatMessage message="Hello how are you?" type="receive" />
+            <ChatMessage message="Hello how are you?" type="sent" />
+          </div>
+        </div>
+        <div className="h-[100px] bg-gray-700 flex justify-center items-center p-3">
+          <input
+            type="text"
+            className="w-full bg-transparent text-white p-2 rounded-md border-2 border-white focus:outline-none focus:border-blue-500"
+            placeholder="Enter your name"
+          />
+          <button className="bg-violet-600 text-white px-3 py-2 rounded-md ml-2">
+            Send
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -1,6 +1,15 @@
 import express from "express";
 import { createServer } from "http";
 import { Server } from "socket.io";
+import { LlamaModel, LlamaContext, LlamaChatSession } from "node-llama-cpp";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+const model = new LlamaModel({
+  modelPath: path.join(__dirname, "models", "codellama-13b.Q3_K_M.gguf"),
+});
+const context = new LlamaContext({ model });
+const session = new LlamaChatSession({ context });
 
 const app = express();
 const server = createServer(app);
@@ -8,6 +17,6 @@ const io = new Server(server);
 
 const PORT = process.env.PORT || 3000;
 
- server.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
