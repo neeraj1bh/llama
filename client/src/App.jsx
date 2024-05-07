dacf6617a88b4c9fda60be9a4bd21bd4002e097f
Neeraj Bhatt <neeraj1bh@gmail.com>
2024-05-07 13:31:10 +0530

    chore: refactored workflow and connected stuff

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,6 @@
+import { useEffect, useState } from "react";
+import { io } from "socket.io-client";
+
 function ChatMessage({ message, type }) {
   return (
     <div
@@ -5,7 +8,7 @@ function ChatMessage({ message, type }) {
         type === "sent" ? "justify-start" : "justify-end"
       }`}
     >
-    <div
+      <div
         className={`${
           type === "sent"
             ? "bg-violet-400  text-white rounded-tl-lg"
@@ -19,23 +22,54 @@ function ChatMessage({ message, type }) {
 }
 
 function App() {
+  const [socket, setSocket] = useState(null);
+  const [inputMessage, setInputMessage] = useState("");
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    const newSocket = io("http://localhost:3000");
+    setSocket(newSocket);
+
+    newSocket.on("message", (message) => {
+      setMessages((prevMessages) => [
+        ...prevMessages,
+        { type: "received", message },
+      ]);
+    });
+
+    return () => newSocket.close();
+  }, []);
+
+  const sendMessage = () => {
+    setMessages((prevMessages) => [
+      ...prevMessages,
+      { type: "sent", message: inputMessage },
+    ]);
+    socket.emit("message", inputMessage);
+  };
+
   return (
     <div className="p-5 h-screen bg-black">
       <div className="container mx-auto bg-gray-900 h-full flex flex-col">
         <div className="flex-1 flex flex-row items-end p-3">
           <div className="w-full">
             <ChatMessage message="Hello how are you?" type="sent" />
-            <ChatMessage message="Hello how are you?" type="receive" />
+            <ChatMessage message="Hello how are you?" type="received" />
             <ChatMessage message="Hello how are you?" type="sent" />
           </div>
         </div>
         <div className="h-[100px] bg-gray-700 flex justify-center items-center p-3">
           <input
             type="text"
+            value={inputMessage}
+            onChange={(e) => setInputMessage(e.target.value)}
             className="w-full bg-transparent text-white p-2 rounded-md border-2 border-white focus:outline-none focus:border-blue-500"
             placeholder="Enter your name"
           />
-          <button className="bg-violet-600 text-white px-3 py-2 rounded-md ml-2">
+          <button
+            onClick={sendMessage}
+            className="bg-violet-600 text-white px-3 py-2 rounded-md ml-2"
+          >
             Send
           </button>
         </div>
