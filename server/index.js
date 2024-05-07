cf5bdcffe07caa43332b571162295cd03f071726
Neeraj Bhatt <neeraj1bh@gmail.com>
2024-05-07 13:30:04 +0530

    chore: refactored workflow and connected stuff

--- server/index.js
+++ server/index.js
@@ -2,18 +2,41 @@ import express from "express";
 import { createServer } from "http";
 import { Server } from "socket.io";
 import { LlamaModel, LlamaContext, LlamaChatSession } from "node-llama-cpp";
+import { fileURLToPath } from "url";
+import path from "path";
+import cors from "cors";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const model = new LlamaModel({
-  modelPath: path.join(__dirname, "models", "codellama-13b.Q3_K_M.gguf"),
+  modelPath: path.join(__dirname, "models", "notus-7b-v1.Q4_K_M.gguf"),
 });
 const context = new LlamaContext({ model });
 const session = new LlamaChatSession({ context });
 
 const app = express();
+
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
 const server = createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+io.on("connection", (soc) => {
+  console.log("Connected");
+  soc.on("message", async (message) => {
+    console.log(message);
+    const response = await session.prompt(message);
+    soc.emit("message", response);
+  });
+});
 
 const PORT = process.env.PORT || 3000;
 
